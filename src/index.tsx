c30f397193e71e26d15cfbf8e232e743d5d058b9
sunny_gangnam <leeseonhee17@gmail.com>
2024-03-20 11:31:34 +0900

    환경설정2

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
-const rootElement = document.getElementById("root")!;
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement );
 
 const client = new QueryClient();
 
