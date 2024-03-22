0a8e2db4418c36ac55f4ba2818874874643f87a2
LEE SEONHEE <leeseonhee17@gmail.com>
2024-03-22 09:32:12 +0900

    리스트 화면 디자인 변경 및 라우터경로변경 및 리스트업데이트부분 수정

--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -1,19 +1,34 @@
 import { useNavigate, Routes, Route } from "react-router-dom";
 import styled from "styled-components";
 import List from "../routes/List";
+import { motion }  from "framer-motion";
 
 const Navi = styled.div`
     margin-top : 20px;
 `;
 
-const Btn = styled.button`
+const Btn = styled(motion.button)`
     background-color : black;
     color : white;
     font-size : 1em;
-    margin : 10px;
+    margin: 20px 10px 10px 10px;
     border:0px;
 `;
 
+const Circle = styled(motion.div)`
+    background-color: #c00;
+    border-radius: 25px;  
+    width:10px ;
+    height:10px ;
+    margin : 0 auto;
+`;
+
+const buttonVariants = {
+
+}
+const iconVariants = {
+
+}
 
 function MenuBar () {
 
@@ -32,12 +47,28 @@ function MenuBar () {
     return (
         <>
         <Navi>
-            <Btn onClick={popular} >POPULAR</Btn>
+            <Btn onClick={popular} 
+                 whileTap="tap" 
+                 variants={buttonVariants} >POPULAR</Btn>
+                 
             <Btn onClick={coming} >COMING SOON</Btn>
+                
             <Btn onClick={now} >NOW PLAYING</Btn>
+
+              
+            <Circle whileTap={{ transition : { }  }} />
+                
         </Navi>
+        <Routes>
+          <Route  path="/"  element={<List/>}></Route>  
+          <Route  path="/coming-soon"  element={<List/>}></Route>  
+          <Route  path="/now-playing"  element={<List/>}></Route>
+        </Routes>
         </>  
     );
 };
 
 export default MenuBar;
+
+
+
